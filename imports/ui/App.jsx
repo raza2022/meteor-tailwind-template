9ec0d39259867dc535abcb8b9974736136f8a089
Abdul Hameed <raza2022@gmail.com>
2023-03-28 22:45:48 +0500

    contractor demo app

---
+++ imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+      <h1 className="text-3xl font-bold underline text-center">
+          Contractor!
+      </h1>
+  </div>
+);
